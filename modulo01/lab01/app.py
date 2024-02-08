f16ebac94ad0aad6a2bb40d326c909e5206614f3
adier maldonado <amaldonado1079@arecibointer.edu>
2024-02-08 13:54:14 -0400

    update

--- modulo01/lab01/app.py
+++ modulo01/lab01/app.py
@@ -1,13 +1,29 @@
 print("hello word")
 
+print("--------------------------------")
+
 def mi_suma(a, b):
     resultado = a + b
     return resultado
 
-
 sum = mi_suma(5, 10)
 
-
-
 print(sum)
+
+print("--------------------------------")
+
+print("Nombre:")
+mensaje = 'Jhon Doe'
+print(mensaje)
+
+print("--------------------------------")
+
+print("gpa:")
+gpa = 3.60
+print(gpa)
+
+print("--------------------------------")
+
+isValid = True
+
 
---
+++ modulo01/lab02/app.py
@@ -0,0 +1,6 @@
+mensaje = input("Entre su nombre: ")
+gpa = float(input("Entre su gpa: "))
+edad = int(input("ingrese su edad: "))	
+
+print(f"Hola {mensaje}!!!!, tu gpa es de {gpa}")
+print(f"Tu edad es: {edad}")
